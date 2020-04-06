cc2d051c8b504de4383215fa67de78fa357c5a74
fenwick67 <smileman66@gmail.com>
2020-04-05 22:37:54 -0400

    progress

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-import {Scene, PerspectiveCamera, WebGLRenderer, HemisphereLight, AnimationMixer, Clock} from "three"
+import {Scene, PerspectiveCamera, WebGLRenderer, HemisphereLight, AnimationMixer, Clock, HemisphereLightHelper, DirectionalLight, DirectionalLightHelper, ACESFilmicToneMapping, NoToneMapping, LinearToneMapping, ReinhardToneMapping, Uncharted2ToneMapping, CineonToneMapping, MeshToonMaterial, MeshBasicMaterial} from "three"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import {GUI} from 'three/examples/jsm/libs/dat.gui.module'
 
 var scene = new Scene();
 var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2000 );
@@ -13,13 +14,19 @@ document.body.appendChild( renderer.domElement );
 
 var loader = new GLTFLoader();
 var mixer;
-loader.load('character.glb', function ( gltf ) {
+loader.load('scene.glb', function ( gltf ) {
     console.log(gltf)
     scene.add( gltf.scene );
+    scene.traverse(function(o){
+        if (o.name == 'Plane'){
+            o.material = new MeshBasicMaterial({map:o.material.map})
+        }
+    })
     mixer = new AnimationMixer( gltf.scene );
     var action = mixer.clipAction(  gltf.animations.find(a=>a.name == "walk") );
     action.play()
     ready = true;
+    
     render();
 }, undefined, function ( error ) {
     console.error('dammit')
@@ -42,6 +49,37 @@ hemiLight.color.setHSL( 0.6, 1, 0.6 );
 hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
 hemiLight.position.set( 0, 50, 0 );
 scene.add( hemiLight );
+
+var hemiLightHelper = new HemisphereLightHelper(hemiLight);
+scene.add(hemiLightHelper);
+
+// dir light
+
+var dirLight = new DirectionalLight( 0xffffff, 1 );
+dirLight.color.setHSL( 0.1, 1, 0.95 );
+dirLight.position.set( -0.1, 1.75, -1 );
+dirLight.position.multiplyScalar( 100 );
+scene.add( dirLight );
+
+dirLight.castShadow = true;
+
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+
+var d = 50;
+
+dirLight.shadow.camera.left = - d;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = - d;
+
+dirLight.shadow.camera.far = 3500;
+dirLight.shadow.bias = - 0.0001;
+
+var dirLightHeper = new DirectionalLightHelper( dirLight, 10 );
+scene.add( dirLightHeper );
+
+renderer.shadowMap.enabled = true;
 
 function onWindowResize() {
 
@@ -66,3 +104,34 @@ function render(){
 
     window.requestAnimationFrame(render);
 }
+
+var toneMappingOptions = {
+    None: NoToneMapping,
+    Linear: LinearToneMapping,
+    Reinhard: ReinhardToneMapping,
+    Uncharted2: Uncharted2ToneMapping,
+    Cineon: CineonToneMapping,
+    ACESFilmic: ACESFilmicToneMapping
+};
+var params = {exposure: 1.5, whitePoint: 1.0, toneMapping:'ACESFilmic'};
+var gui = new GUI();
+
+gui.add(params, 'toneMapping', Object.keys(toneMappingOptions))
+    .onChange(function(){
+        renderer.toneMapping = toneMappingOptions[params.toneMapping]
+        
+        scene.traverse(function(o){
+            if (o.material){
+                o.material.needsUpdate = true;
+            }
+        })
+    })
+gui.add(params, 'exposure', 0, 2)
+    .onChange(function(){
+        renderer.toneMappingExposure = params.exposure;
+    })
+gui.add(params, 'whitePoint', 0, 2)
+.onChange(function(){
+    renderer.toneMappingWhitePoint = params.whitePoint;
+})
+gui.open()
