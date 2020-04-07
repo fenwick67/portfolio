f287cc672ca474d5977c9b093ceeaa9e238cb2f1
fenwick67 <smileman66@gmail.com>
2020-04-06 22:51:48 -0400

    start modularizing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import {GUI} from 'three/examples/jsm/libs/dat.gui.module'
 import {OutlineEffect} from "three/examples/jsm/effects/OutlineEffect.js"
+import {getOutdoorLighting} from "./lib/entities"
 
 var scene = new Scene();
 var camera = new PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 2000 );
@@ -52,49 +53,14 @@ loader.load('scene.glb', function ( gltf ) {
 
 var controls = new OrbitControls( camera, renderer.domElement );
 // controls.addEventListener( 'change', render ); // use if there is no animation loop
-controls.minDistance = 100;
-controls.maxDistance = 200;
+controls.minDistance = 10;
+controls.maxDistance = 20;
 controls.target.set( 0, 0, - 0.2 );
 controls.update();
 
 window.addEventListener( 'resize', onWindowResize, false );
 
-
-var hemiLight = new HemisphereLight( 0xffffff, 0xffffff, 0.9 );
-hemiLight.color.setHSL( 0.6, 1, 0.6 );
-hemiLight.groundColor.setHSL( 0.095, 0.7, 0.75 );
-hemiLight.position.set( 0, 50, 0 );
-scene.add( hemiLight );
-
-var hemiLightHelper = new HemisphereLightHelper(hemiLight);
-scene.add(hemiLightHelper);
-
-// dir light
-
-var dirLight = new DirectionalLight( 0xffffff, 0.7 );
-dirLight.color.setHSL( 0.1, 1, 0.75 );
-dirLight.position.set( -0.1, 1.75, 1 );
-dirLight.position.multiplyScalar( 50 );
-scene.add( dirLight );
-
-dirLight.castShadow = true;
-
-dirLight.shadow.mapSize.width = 1024;
-dirLight.shadow.mapSize.height = 1024;
-
-var d = 200;
-
-dirLight.shadow.camera.left = - d;
-dirLight.shadow.camera.right = d;
-dirLight.shadow.camera.top = d;
-dirLight.shadow.camera.bottom = - d;
-
-dirLight.shadow.camera.far = 3500;
-dirLight.shadow.bias = - 0.0001;
-dirLight.shadow.radius = 5;
-
-var dirLightHeper = new DirectionalLightHelper( dirLight, 10 );
-scene.add( dirLightHeper );
+getOutdoorLighting().forEach(l=>scene.add(l))
 
 renderer.shadowMap.type = VSMShadowMap;
 renderer.shadowMap.enabled = true;
@@ -123,8 +89,8 @@ function render(){
 
     scene.traverse(function(o){
         if (o.name == 'Armature'){
-            o.position.z = Math.sin(clock.getElapsedTime()) * 30
-            o.position.x = Math.cos(clock.getElapsedTime()) * 30
+            o.position.z = Math.sin(clock.getElapsedTime()) * 3
+            o.position.x = Math.cos(clock.getElapsedTime()) * 3
             o.setRotationFromAxisAngle(new Vector3(0, 1, 0), -clock.getElapsedTime())
         }
     })
