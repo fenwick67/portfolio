aacdca3a2fc0d42a9b3c445a9acbdeb6b5d33739
Drew F. Harwell <harwelldrewf@johndeere.com>
2020-04-09 18:24:26 -0400

    add boom box and casette

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import {Scene, PerspectiveCamera, WebGLRenderer, HemisphereLight, AnimationMixer, Clock, HemisphereLightHelper, DirectionalLight, DirectionalLightHelper, ACESFilmicToneMapping, NoToneMapping, LinearToneMapping, ReinhardToneMapping, Uncharted2ToneMapping, CineonToneMapping, MeshToonMaterial, MeshBasicMaterial, OrthographicCamera, AmbientLight, PCFSoftShadowMap, VSMShadowMap, Vector3} from "three"
+import {Scene, PerspectiveCamera, WebGLRenderer, HemisphereLight, AnimationMixer, Clock, HemisphereLightHelper, DirectionalLight, DirectionalLightHelper, ACESFilmicToneMapping, NoToneMapping, LinearToneMapping, ReinhardToneMapping, Uncharted2ToneMapping, CineonToneMapping, MeshToonMaterial, MeshBasicMaterial, OrthographicCamera, AmbientLight, PCFSoftShadowMap, VSMShadowMap, Vector3, sRGBEncoding, Vector2} from "three"
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import {GUI} from 'three/examples/jsm/libs/dat.gui.module'
@@ -8,13 +8,40 @@ import {getOutdoorLighting, getEntity, entities} from "./lib/entities"
 import { loadAll } from "./lib/loader";
 import { waterMaterial } from "./lib/waterShader";
 
+
 var scene = new Scene();
 var camera = new PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 2000 );
 
 var renderer = new WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// THIS is why my stuff all looked different than in Blender
+renderer.outputEncoding = sRGBEncoding;
 
+function resizeRenderer(){
+    var targetResolutionY = 300// target Y resolution
+    var pixelSize = window.innerHeight / targetResolutionY;
+    pixelSize = Math.floor(pixelSize);
+    pixelSize = Math.max(pixelSize, 1);
+
+    var rendererX = Math.ceil(window.innerWidth / pixelSize);
+    var rendererY = Math.ceil(window.innerHeight / pixelSize);
+
+    renderer.setSize( rendererX, rendererY);
+    var styleW = Math.round(rendererX * pixelSize );
+    var styleH = Math.round(rendererY * pixelSize );
+    var styleImRendering = `image-rendering:optimizeSpeed;
+        image-rendering:-moz-crisp-edges; 
+        image-rendering:-o-crisp-edges;
+        image-rendering:-webkit-optimize-contrast;
+        image-rendering:optimize-contrast;
+        image-rendering:crisp-edges;
+        image-rendering:pixelated;
+        -ms-interpolation-mode:nearest-neighbor;`;    
+    renderer.domElement.style = `width: ${styleW}; height: ${styleH}; ${styleImRendering}`;
+
+}
+
+document.body.appendChild( renderer.domElement );
+resizeRenderer();
 
 var loader = new GLTFLoader();
 loadAll(function(err){
@@ -30,6 +57,7 @@ var controls = new OrbitControls( camera, renderer.domElement );
 controls.minDistance = 10;
 controls.maxDistance = 100;
 controls.target.set( 0, 0, - 0.2 );
+controls.maxPolarAngle = 1.0
 controls.update();
 
 window.addEventListener( 'resize', onWindowResize, false );
@@ -47,12 +75,11 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    // render();
+    resizeRenderer()
 
 }
 var clock = new Clock();
+clock.start(); 
 function render(){
     var delta = clock.getDelta();
 
